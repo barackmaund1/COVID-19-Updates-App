585dc3db288c646786bd9a2dd8b89c6ef1b8be3b
barackmaund1 <pmaundu49@gmail.co>
2020-05-29 15:38:08 +0300

    deployment

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app.model import User
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
